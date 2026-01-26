8116f867f2c44093a2f54ee785bf3a41f4c63aa6
Abhinav Shrestha <139767616+abhinavshr@users.noreply.github.com>
2026-01-26 21:16:23 +0545

    feat: added the coludinary for the profile picture

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,18 +2,19 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const db = require('../config/db');
+const cloudinary = require('../config/cloudinary');
+const upload = require('../middlewares/upload.middleware');
 
-// REGISTER API
-router.post('/register', async (req, res) => {
+// REGISTER API WITH PROFILE PIC
+router.post('/register', upload.single('profile_pic'), async (req, res) => {
   try {
     const { username, email, password, full_name } = req.body;
 
-    // 1. Validate input
     if (!username || !email || !password) {
       return res.status(400).json({ message: 'All required fields missing' });
     }
 
-    // 2. Check if user exists
+    // Check existing user
     const checkQuery = 'SELECT * FROM users WHERE email = ? OR username = ?';
     db.query(checkQuery, [email, username], async (err, result) => {
       if (err) return res.status(500).json({ error: err });
@@ -22,25 +23,37 @@ router.post('/register', async (req, res) => {
         return res.status(409).json({ message: 'User already exists' });
       }
 
-      // 3. Hash password
+      // Hash password
       const salt = await bcrypt.genSalt(10);
       const hashedPassword = await bcrypt.hash(password, salt);
 
-      // 4. Insert user
+      let profilePicUrl = null;
+
+      // Upload image to Cloudinary (if exists)
+      if (req.file) {
+        const uploadResult = await cloudinary.uploader.upload(req.file.path, {
+          folder: 'instagram_profiles',
+        });
+
+        profilePicUrl = uploadResult.secure_url;
+      }
+
+      // Insert user
       const insertQuery = `
-        INSERT INTO users (username, email, password, full_name)
-        VALUES (?, ?, ?, ?)
+        INSERT INTO users (username, email, password, full_name, profile_pic)
+        VALUES (?, ?, ?, ?, ?)
       `;
 
       db.query(
         insertQuery,
-        [username, email, hashedPassword, full_name],
+        [username, email, hashedPassword, full_name, profilePicUrl],
         (err, result) => {
           if (err) return res.status(500).json({ error: err });
 
           res.status(201).json({
             message: 'User registered successfully',
-            user_id: result.insertId
+            user_id: result.insertId,
+            profile_pic: profilePicUrl
           });
         }
       );
